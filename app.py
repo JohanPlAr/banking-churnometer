f019617716dc7353080bc863bafc88070e5f533e
JohanPlAr <johan.arkert@gmail.com>
2024-03-08 13:50:59 +0100

    Add Streamlit page_predict_churn

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from app_pages.multipage import MultiPage
 from app_pages.page_summary import page_summary_body
 from app_pages.page_churned_customer_study import page_churned_customer_study_body
+from app_pages.page_predict_churn import page_predict_churn_body
 
 # load pages scripts
 
@@ -11,7 +12,7 @@ app = MultiPage(app_name= "Banking-Churnometer") # Create an instance of the app
 app.add_page("Quick Project Summary", page_summary_body)
 app.add_page("Customer Base Churn Study", page_churned_customer_study_body)
 app.add_page("Prospect Churnometer", page_summary_body)
-app.add_page("ML: Prospect Churn", page_summary_body)
+app.add_page("ML: Prospect Churn", page_predict_churn_body)
 app.add_page("ML: Prospect Tenure", page_summary_body)
 app.add_page("ML: Cluster Analysis", page_summary_body)
 
