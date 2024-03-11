45b1b297151722f0d9fa18e5bcf8ee35f476b400
JohanPlAr <johan.arkert@gmail.com>
2024-03-11 10:38:28 +0100

    Add Streamlit page for tenure prospect

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from app_pages.page_churned_customer_study import page_churned_customer_study_bo
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
 from app_pages.page_prospect import page_prospect_body
 from app_pages.page_predict_churn import page_predict_churn_body
+from app_pages.page_prospect_tenure import page_predict_tenure_body
 
 # load pages scripts
 
@@ -16,7 +17,7 @@ app.add_page("Customer Base Churn Study", page_churned_customer_study_body)
 app.add_page("Prospect Churnometer", page_prospect_body)
 app.add_page("Project Hypothesis and Validation", page_project_hypothesis_body)
 app.add_page("ML: Prospect Churn", page_predict_churn_body)
-app.add_page("ML: Prospect Tenure", page_summary_body)
+app.add_page("ML: Prospect Tenure", page_predict_tenure_body)
 app.add_page("ML: Cluster Analysis", page_summary_body)
 
 app.run() # Run the  app
